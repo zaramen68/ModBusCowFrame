92b74a234bffe4b29ca52ef86063706a2cd83ac3
Michael Zaramensky <m.zaramensky@tochka.ai>
2025-07-21 17:21:34 +0300

    refactoring

--- src/cow_modbus/modbus_client.py
+++ src/cow_modbus/modbus_client.py
@@ -20,27 +20,6 @@ from cow_modbus.protocol.frame_message import Message
 
 MAX_CONNECT = 4
 
-# """ Структура сообщения от рамки, заполняется при каждом опросе рамки"""
-# @dataclass
-# class Message:
-#     """Время *20 ms прошедшее с последнего срабатывания метки fdx. макимальное значение 255"""
-#     fdx_time: int | None = None
-#     """id fdx метки"""
-#     fdx_id: int | None = None
-#     """rssi fdx метки"""
-#     fdx_rssi: int | None = None
-#     """Время *20 ms прошедшее с последнего срабатывания метки hdx. макимальное значение 255"""
-#     hdx_time: int | None = None
-#     """id hdx метки"""
-#     hdx_id: int | None = None
-#     """rssi hdx метки"""
-#     hdx_rssi: int | None = None
-#     """Состояние ответа. Если ошибка -  false"""
-#     is_valid: bool = False
-#     """Текст последней ошибки"""
-#     last_error: str | None = None
-#     """ Время создания сообщения """
-#     time_stamp: float
 
 class ModbusClient(Thread):
     
